7942ff5523c41ca7f684d606b74e2741dda690c0
nesto <ernest.nied@gmail.com>
2025-10-22 19:23:53 +0200

    résolution du proble d'affichage des objets (ici un dictionnaire), l'affichage du résultat d'une requete de gare marche désormais parfaitement

--- gares/garesJS.js
+++ gares/garesJS.js
@@ -12,29 +12,44 @@ function getLines(){
     });  
 }
 
+function json2Table(jsonData, idTable){
+
+    function tab2str(tab){
+        strvtab = "";
+        for(v of Object.values(tab)){
+            strvtab += `${v},`
+        }
+
+        return strvtab ;
+    }
+
+    //on met les entetes dans l'element table
+    let htmlTabGares = "<table><tr>";
+    for (var clef of Object.keys(jsonData[0])) {//il doit y avoir plus simple
+        htmlTabGares += `<td>${clef}</td>`;// ` pour avoi les variables dans la string
+    }
+    htmlTabGares += "</tr>";
+
+    for(var gare of jsonData){
+        htmlTabGares += "<tr>"
+        for (var info of Object.values(gare)){
+            if (typeof(info) == "object"){
+                htmlTabGares+= `<td>${tab2str(info)}</td>`;
+            } else{
+                htmlTabGares += `<td>${info}</td>`;
+            }
+        }
+        htmlTabGares += "</tr>"
+    }//il y a des doublons à enlever et les coordonnées ne s'affichent pas bien car c'est un dico
+
+    htmlTabGares += "</table>";
+    document.getElementById(idTable).innerHTML = htmlTabGares
+}
+
 function garesAccessibles(gareID){
     console.log("gares depuis :",gareID);
-    let htmlTabGares = "<table><tr>"
-    let tabResGares;
+
     $.getJSON("/gares/garesAtteignables/"+gareID, dataGaresAcces =>{//dataGaresAcces ne peut pas etre vide car une garere ne peut pas être isollée
-        //a mettre dans une fonction
-        //on met les entetes dans l'ellement table
-        //console.log(dataGaresAcces[0]);
-        let htmlTabGares = "<table><tr>";
-        for (var clef of Object.keys(dataGaresAcces[0])) {//il doit y avoir plus simple
-            htmlTabGares += `<td>${clef}</td>`;// ` pour avoi les variables dans la string
-        }
-        htmlTabGares += "</tr>";
-
-        for(var gare of dataGaresAcces){
-            htmlTabGares += "<tr>"
-            for (var info of Object.values(gare)){
-                htmlTabGares += `<td>${info}</td>`
-            }
-            htmlTabGares += "</tr>"
-        }//il y a des doublons à enlever et les coordonnées ne s'affichent pas bien car c'est un dico
-
-        htmlTabGares += "</table>";
-        document.getElementById("tableauGares").innerHTML = htmlTabGares
+        json2Table(dataGaresAcces, "tableauGares")
     });
 }
